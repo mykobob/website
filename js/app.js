fec74006125abc27f2acf2e28b357faa1474d9a7
mykobob <michael.eagle@utexas.edu>
2016-12-16 00:51:26 -0600

    added tempt bot to projects page. also fixed nav bar to be solid

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-var load_email = function() {
+var load_email = function () {
 	var arr = [109, 105, 99, 104, 97, 101, 108, 46, 101, 97, 103, 108, 101, 64, 117, 116, 101, 120, 97, 115, 46, 101, 100, 117];
     var em = "";
     var arr_length = arr.length;
-    for(var i=0;i<arr_length;i++)em+=String.fromCharCode(arr[i]);
+	var i;
+	
+    for (i=0;i<arr_length;i++) em+=String.fromCharCode(arr[i]);
 
     $("#email").html("Email");
     $("#email").attr("href", "mailto:"+em);
